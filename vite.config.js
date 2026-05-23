4ae621ff0127b6609f3edd2629c45d14c7c196e8
Paul Nwaoduah <nwaoduahpaul@gmail.com>
2026-05-23 13:58:57 +0100

    to vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Spender-Dashboard/",
+  base: process.env.VERCEL ? '/' : "/Spender-Dashboard/",
   plugins: [react()],
 })
